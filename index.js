ded44901e79a72c94babddf0eac209f15e64475c
Steven Luzynski <sluzynsk@cisco.com>
2021-10-19 12:57:58 -0500

    missing semicolon. oops.

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ framework.on('attachmentAction', function (bot, trigger) {
 
   if (action == "sub_theatre") {
     // They picked a theatre, so stick that value in the fact block
-    bot.say(`Looks like you chose the ${JSON.stringify(trigger.attachmentAction.inputs.choices)}.`)
+    bot.say(`Looks like you chose the ${JSON.stringify(trigger.attachmentAction.inputs.choices)}.`);
     new_card_step2.facts[0].value = trigger.attachmentAction.inputs.choices;
     bot.sendCard(new_card_step2,
       "Your client does not support buttons and cards. Please update and try again.");
